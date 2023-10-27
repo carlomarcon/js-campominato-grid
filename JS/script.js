8989f4fcfa342feae164c57c8d6a2284eb311301
carlomarcon <carlomarcon1998@gmail.com>
2023-10-27 16:28:58 +0200

    ADD: JS GAME

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+const box = document.querySelector(".box");
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+// EVOCARE FUNZIONE
+
+generator(100);
+Listener(100);
+
+button.addEventListener("click", function () {
+  box.style.display = "flex";
+  h1.style.display = "none";
+});
+
+// FUNZIONI
+
+function generator(numberofcell) {
+  let insert = "";
+
+  for (i = 1; i <= numberofcell; i++) {
+    insert += "<div class='cell'>" + i + "</div>";
+  }
+  return (box.innerHTML = insert);
+}
+
+function Listener(numberofcell) {
+  let cell = document.querySelectorAll(".cell");
+  for (i = 1; i < numberofcell; i++) {
+    cell[i].addEventListener("click", function () {
+      console.log(this.innerHTML);
+    });
+  }
+}
+
+// let test = (document.createElement("h1").innerHTML = "hey");
+
+// let cell = document.querySelector(".box");
+// cell.append(test);
